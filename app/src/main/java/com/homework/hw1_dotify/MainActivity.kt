30c41098ca17bb8d5efb95d1b30e71c416bf3774
Patrin <patrin.sinteppadon@gmail.com>
2020-04-13 20:06:30 -0700

    implement switching user accounts

--- app/src/main/java/com/homework/hw1_dotify/MainActivity.kt
+++ app/src/main/java/com/homework/hw1_dotify/MainActivity.kt
@@ -2,24 +2,68 @@ package com.homework.hw1_dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var numPlays = Random.nextInt(1, 1001) // number of times the song has been played
+    private var loggedIn = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        randomizeSongPlays()
+        // init number of plays
+        btnPlay.setOnClickListener {
+            numPlays++
+            updatePlays()
+        }
+
+        // init "change user" button
+        btnChangeUser.setOnClickListener {
+            if (loggedIn) {
+                etUsername.text.clear()
+                etUsername.visibility = View.VISIBLE
+                tvUsername.visibility = View.INVISIBLE
+                btnChangeUser.text = getString(R.string.apply) // displays as "Apply"
+            } else {
+                tvUsername.text = etUsername.text
+                tvUsername.visibility = View.VISIBLE
+                etUsername.visibility = View.INVISIBLE
+                btnChangeUser.text = getString(R.string.change_user) // displays as "Change User"
+            }
+
+            loggedIn = !loggedIn
+        }
+
+        // init "previous song" button
+        btnPrev.setOnClickListener {
+            displayToast("Skipping to previous track")
+        }
+
+        // init "next song" button
+        btnNext.setOnClickListener {
+            displayToast("Skipping to next track")
+        }
+
+        // init screen
+        updatePlays()
     }
 
     /**
-     * set text for tvSongPlays to a random number between 1 and 1000
-     * runs on startup
+     * binds current num of song plays to the screen
      */
-    private fun randomizeSongPlays() {
-        var tvSongPlays = findViewById<TextView>(R.id.tvSongPlays)
-        tvSongPlays.text = Random.nextInt(1, 1001).toString() + " plays"
+    private fun updatePlays() {
+        tvSongPlays.text = "$numPlays ${getString(R.string.plays)}" // displays as: "x plays"
+    }
+
+    /**
+     * prototype function.
+     * displays message explaining intended behavior of final product.
+     */
+    private fun displayToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
