e8d719ad27cc38b886c29a8e35ca06c97bdc0580
Patrin <patrin.sinteppadon@gmail.com>
2020-04-13 17:24:59 -0700

    begin repo

---
+++ app/src/main/java/com/homework/hw1_dotify/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.hw1_dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
