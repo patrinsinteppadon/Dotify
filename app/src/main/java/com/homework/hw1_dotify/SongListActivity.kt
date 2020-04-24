cd3d90d075e705052a020d49c78aabe75bea668b
Patrin <patrin.sinteppadon@gmail.com>
2020-04-23 18:14:26 -0700

    install songlist and recyclerview libraries

---
+++ app/src/main/java/com/homework/hw1_dotify/SongListActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.hw1_dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_list)
+    }
+}
